86fc6db55aa48f9dbc0adc49bbfe6279289f7bdd
ChrisHishmeh <98780864+ChrisHishmeh@users.noreply.github.com>
2022-02-10 09:10:44 -0500

    contributor 2
    UI stuff

--- groupapp.R
+++ groupapp.R
@@ -1,12 +1,23 @@
-
-
 library(shiny)
+library(fpp3)
+data("tourism")
+
+data <- subset(tourism, Region=="Alice Springs")
 
 ui <- fluidPage(
-  
+  selectInput(
+    inputId = "region",
+    label="Select Region",
+    choice=unique(tourism$Region)
+  ),
+plotOutput("plotted_series")
 )
 
 server <- function(input, output, session) {
+  output$plotted_series <- renderPlot({
+    
+  })
+  
   
 }
 
